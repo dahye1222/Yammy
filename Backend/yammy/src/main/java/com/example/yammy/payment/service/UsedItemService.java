5cbd9cc89329dd5bff6d37ab7c2c900bd6019f6e
김선우 <dora01622@naver.com>
2025-10-22 12:53:22 +0900

    feat: 백엔드 폴더 초기 세팅 업로드

---
+++ Backend/yammy/src/main/java/com/example/yammy/payment/service/UsedItemService.java
@@ -0,0 +1,4 @@
+package com.example.yammy.payment.service;
+
+public class UsedItemService {
+}
---
+++ Backend/yammy/src/test/java/com/example/payment/YammyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yammy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YammyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
