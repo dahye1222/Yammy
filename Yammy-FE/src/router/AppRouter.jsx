bc23352e91d771c9d2b45162d798a9e026daa281
김태영 <erd57819@gmail.com>
2025-10-29 15:08:11 +0900

    fix:로그인 프론트

--- Yammy-FE/src/router/AppRouter.jsx
+++ Yammy-FE/src/router/AppRouter.jsx
@@ -6,18 +6,26 @@ import UsedItemPage from "../useditem/UsedItemPage";
 import UsedItemDetail from "../useditem/components/UsedItemDetail"
 import UsedItemEdit from "../useditem/components/UsedItemEdit"
 import UsedItemCreate from "../useditem/components/UsedItemCreate"
+import LoginPage from "../auth/LoginPage";
+import SignupPage from "../auth/SignupPage";
 
 export default function AppRouter() {
   return (
     <Routes>
+      {/* Auth Routes */}
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+
+      {/* SNS Routes */}
+      <Route path="/" element={<SNSPage />} />
+      <Route path="/post/:postId/comments" element={<CommentPage />} />
+      <Route path="/user/:userId" element={<UserProfile />} />
+
+      {/* UsedItem Routes */}
       <Route path="/useditem" element={<UsedItemPage />} />
       <Route path="/useditem/:id" element={<UsedItemDetail />} />
       <Route path="/useditem/edit/:id" element={<UsedItemEdit />} />
       <Route path="/useditem/create" element={<UsedItemCreate />} />
-      <Route path="/" element={<SNSPage />} />
-      <Route path="/post/:postId/comments" element={<CommentPage />} />
-      <Route path="/user/:userId" element={<UserProfile />} />
-  
     </Routes>
   );
 }
